b2219b9defd70909ca36a7a36b56b1e66a42d8f3
rhaughey <rfhaughey@gmail.com>
2019-10-03 16:31:49 -0400

    Tweaks to maven wrapper

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+/**
+ * Created by rfh on 9/18/2019
+ */
+public class HelloWorldTest {
+
+    public void testHello() {
+        System.out.println("TEST - Hello...");
+    }
+
+    public void testWorld() {
+        System.out.println("TEST - World!!!");
+    }
+}
